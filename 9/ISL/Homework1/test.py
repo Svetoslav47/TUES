b90463b868e84fdb93f8c930837752c0c450a072
Svetoslav47 <svetoslav2806@gmail.com>
2022-11-22 19:21:16 +0200

    changed a folder

--- 9/ISL/Homework1/test.py
+++ 9/ISL/Homework1/test.py
@@ -1,6 +1,22 @@
-def test(x, y, z):
-    return x * y * z
+spam = ""
+p = True
+while p is True:
+    spam = input("enter a list of numbers only or simbols only ")
+    _list = spam.split(",")
+    intowe = False
+    charowe = False
+    for i in range(len(_list)):
+        try:
+            _list[i] = _list[i].isdigit()
+            intowe = True
+        except ValueError:
+            charowe = True
+    if intowe is True and charowe is True:
+        p = True
+        print("there are simbols and numbers , please choose one !!!!")
+    else:
+        p = False
 
-
-result = test(2, 3, 4)
-print(result)
+_list = list(set(_list))
+for x in _list:
+    print(x)
