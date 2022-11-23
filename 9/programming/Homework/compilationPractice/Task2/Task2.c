c6930d0fedf94f00d93ec487650a30e850a644a3
Svetoslav47 <svetoslav2806@gmail.com>
2022-11-23 21:19:33 +0200

    redact

--- 9/programming/Homework/compilationPractice/Task2/Task2.c
+++ 9/programming/Homework/compilationPractice/Task2/Task2.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
-int main() {
+int main()
+{
 	int x = 12, y = 18;
-    //the least common multiple is atleast the same as the bigger number so we start from there
+	// the least common multiple is atleast the same as the bigger number so we start from there
 	int biggest = (x > y) ? x : y;
 
-    //we inciment it until we find a LCM
-	while (1) {
-		if (biggest % x == 0 && biggest % y == 0) {
+	// we inciment it until we find a LCM
+	while (1)
+	{
+		if (biggest % x == 0 && biggest % y == 0)
+		{
 			printf("The LCM for %d and %d is %d.", x, y, biggest);
 			break;
 		}
