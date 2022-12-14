cf1f395526359ec83334d0e7223ad30aedb840fe
Svetoslav47 <svetoslav2806@gmail.com>
2022-12-14 19:55:04 +0200

    error in safeint

--- 9/programming/Homework/dataInMemory/task2/safeint.c
+++ 9/programming/Homework/dataInMemory/task2/safeint.c
@@ -1,99 +1,121 @@
 #include "safeint.h"
 #include <limits.h>
 
-
-struct SafeResult safeadd(int x, int y){
+struct SafeResult safeadd(int x, int y)
+{
     struct SafeResult result;
-    if (x > 0 && y > 0 && x > INT_MAX - y){
+    if (x > 0 && y > 0 && x > INT_MAX - y)
+    {
         result.errorflag = 0;
         result.value = 0;
     }
-    else if (x < 0 && y < 0 && x < INT_MIN - y){
+    else if (x < 0 && y < 0 && x < INT_MIN - y)
+    {
         result.errorflag = 0;
         result.value = 0;
     }
-    else{
+    else
+    {
         result.errorflag = 1;
         result.value = x + y;
     }
     return result;
 }
 
-struct SafeResult safesubtract(int x, int y){
+struct SafeResult safesubtract(int x, int y)
+{
     struct SafeResult result;
-    if (x > 0 && y < 0 && x > INT_MAX + y){
+    if (x > 0 && y < 0 && x > INT_MAX + y)
+    {
         result.errorflag = 0;
         result.value = 0;
     }
-    else if (x < 0 && y > 0 && x < INT_MIN + y){
+    else if (x < 0 && y > 0 && x < INT_MIN + y)
+    {
         result.errorflag = 0;
         result.value = 0;
     }
-    else{
+    else
+    {
         result.errorflag = 1;
         result.value = x - y;
     }
     return result;
 }
 
-struct SafeResult safemultiply(int x, int y){
+struct SafeResult safemultiply(int x, int y)
+{
     struct SafeResult result;
-    if (x > 0 && y > 0 && x > INT_MAX / y){
+    if (x > 0 && y > 0 && x > INT_MAX / y)
+    {
         result.errorflag = 0;
         result.value = 0;
     }
-    else if (x < 0 && y < 0 && x < INT_MAX / y){
+    else if (x < 0 && y < 0 && x < INT_MAX / y)
+    {
         result.errorflag = 0;
         result.value = 0;
     }
-    else if (x > 0 && y < 0 && x < INT_MIN / y){
+    else if (x > 0 && y < 0 && x > INT_MIN / y)
+    {
         result.errorflag = 0;
         result.value = 0;
     }
-    else if (x < 0 && y > 0 && x < INT_MIN / y){
+    else if (x < 0 && y > 0 && x < INT_MIN / y)
+    {
         result.errorflag = 0;
         result.value = 0;
     }
-    else{
+    else
+    {
         result.errorflag = 1;
         result.value = x * y;
     }
     return result;
 }
 
-struct SafeResult safedivide(int x, int y){
+struct SafeResult safedivide(int x, int y)
+{
     struct SafeResult result;
-    if (y == 0){
+    if (y == 0)
+    {
         result.errorflag = 0;
         result.value = 0;
     }
-    else if (x == INT_MIN && y == -1){
+    else if (x == INT_MIN && y == -1)
+    {
         result.errorflag = 0;
         result.value = 0;
     }
-    else{
+    else
+    {
         result.errorflag = 1;
         result.value = x / y;
     }
     return result;
 }
 
-struct SafeResult safestrtoint(char* str){
+struct SafeResult safestrtoint(char *str)
+{
     struct SafeResult result;
     int i = 0;
     int sign = 1;
     int value = 0;
-    if (str[0] == '-'){
+    if (str[0] == '-')
+    {
         sign = -1;
         i++;
     }
-    while (str[i] != '\0'){
-        if (str[i] < '0' || str[i] > '9'){
+    while (str[i] != '\0')
+    {
+        if (str[i] < '0' || str[i] > '9')
+        {
             result.errorflag = 0;
             result.value = 0;
             return result;
         }
-        if (value > INT_MAX / 10 || (value == INT_MAX / 10 && str[i] - '0' > INT_MAX % 10)){
+        if (value > INT_MAX / 10 || (value == INT_MAX / 10 && str[i] - '0' > INT_MAX % 10))
+        {
             result.errorflag = 0;
             result.value = 0;
             return result;
--- 9/programming/Homework/makefileAndMainArgs/help.c
+++ 9/programming/Homework/makefileAndMainArgs/help.c
@@ -4,5 +4,5 @@ void main()
 {
     printf("strlength:Takes a string and prints its length.\n");
     printf("strconcat:Takes two strings, concatinates them and puts the result in the first string.(Maximum length of a string => 50)\n");
-    printf("strcompare:Takes two strings, compares them and returns the result.\n")
+    printf("strcompare:Takes two strings, compares them and returns the result.\n");
 }
